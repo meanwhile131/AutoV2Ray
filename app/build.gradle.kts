d692bc7464ef067a22947a84ba9f7861a8191bdd
Sasha Chernyakov <sashachernyakov111111@gmail.com>
2026-08-28 18:32:11 +0400

    Fix build and add automatic download of libXray

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import de.undercouch.gradle.tasks.download.Download
+
 plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.android.application)
@@ -12,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "org.dpdns.meanwhile131.autov2ray"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
@@ -36,9 +38,10 @@ android {
     }
 }
 
+val libXrayDir = layout.buildDirectory.dir("lib")
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
-    implementation(fileTree(mapOf("dir" to "lib", "include" to listOf("*.aar", "*.jar"))))
+    implementation(fileTree(mapOf("dir" to libXrayDir.map { it.dir("libxray-android") }, "include" to listOf("*.aar", "*.jar"))))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
@@ -56,4 +59,19 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+}
+val libXrayDownload = tasks.register<Download>("downloadLibXray") {
+    description = "Download libXray"
+    src("https://github.com/XTLS/libXray/releases/latest/download/libxray-android.zip")
+    dest(layout.buildDirectory.file("libxray-android.zip"))
+    onlyIfModified(true)
+}
+val libXray = tasks.register<Copy>("extractLibXray") {
+    description = "Extract libXray archive"
+    dependsOn(libXrayDownload)
+    from(zipTree(libXrayDownload.get().dest))
+    into(libXrayDir)
+}
+tasks.named("preBuild") {
+    dependsOn(libXray)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    id("de.undercouch.download") version "5.7.0"
 }
